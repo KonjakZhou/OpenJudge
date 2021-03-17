be516b21c625f737d33a6eec4f1454bd50ec3cea
KonJakZhou <cuttleb@163.com>
2021-03-17 09:58:27 +0800

    LeetCode剑指offer19

--- Python/LeetCode剑指offer19.py
+++ Python/LeetCode剑指offer19.py
@@ -1,6 +1,5 @@
 class Solution:
     def isMatch(self, s, p):
-
         m = len(s)
         n = len(p)
         flag = [ [False for _ in range(n+1)] for __ in range(m+1) ] 
@@ -13,6 +12,9 @@ class Solution:
             for j in range(1, n+1):
                 if s[i-1] == p[j-1] or p[j-1]==".":
                     flag[i][j] = flag[i-1][j-1]
-                elif p[j-1] == '*' and (p[j-2] == "." or s[i-1] == p[j-2]):
-                    flag[i][j] = flag[i][j-2] or flag[i-1][j]
+                elif p[j-1] == '*' :
+                    flag[i][j] = flag[i][j-2]
+                    if p[j-2] == "." or s[i-1] == p[j-2]:
+                        flag[i][j] |= flag[i-1][j]
+                
         return flag[m][n]
