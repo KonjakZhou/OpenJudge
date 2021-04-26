e29cfe1a0923c9678aa23e88d26909e3d3b23403
KonJakZhou <cuttleb@163.com>
2021-04-26 18:17:19 +0800

    LeetCode1011

--- Python/test.py
+++ Python/test.py
@@ -1,16 +1,3 @@
-import sympy   # 引入解方程的专业模块sympy
+list = []
 
-import time
-
-start = time.time()
-x = sympy.symbols("x")   # 申明未知数"x"
-y = sympy.symbols("y")   # 申明未知数"x"
-
-a = sympy.solve(
-    [
-        x ** 2 + y ** 2 -1,
-        x - y
-    ],[x,y])   # 写入需要解的方程体
-b = [[x.evalf() for x in y ] for y in a]
-print(b)
-print(time.time() - start)
+list[-1].append([1])
