f5bec95e0f964a95462c3102557cc5712623168a
KonJakZhou <cuttleb@163.com>
2021-08-31 01:02:51 +0800

    LeetCode1109

--- LeetCode1109.py
+++ LeetCode1109.py
@@ -4,3 +4,6 @@ class Solution:
         for booking in bookings:
             d[booking[0]-1] += booking[2]
             d[booking[1]] -= booking[2]
+        for i in range(1,n):
+            d[i] = d[i] + d[i-1]
+        return d[:-1]
