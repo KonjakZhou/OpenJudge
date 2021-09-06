5fdc319353ab7bc727f5dc0c87bf61a206da5d03
KonJakZhou <cuttleb@163.com>
2021-09-06 12:18:48 +0800

    nothing

--- test.py
+++ test.py
@@ -1,18 +1,29 @@
-def bisearch(l, r, target, w):
-    if (r - l) <= 1:
-        if w[l] >= target:
-            return l
-        return r
+def C(m, n):
+    ans1 = 1.0
+    for i in range(1, m+1):
+        ans1 = ans1 * i
+    ans2 = 1.0
+    for i in range(1, n+1):
+        ans2 = ans2 * i
+    for i in range(1, m-n+1):
+        ans2 = ans2 * i
 
-    mid = (l + r) >> 1
-    print(l," ", r, " ", mid)
-    if w[mid] >= target:
-        r = mid
-    else:
-        l = mid
-    
-    return bisearch(l,r,target, w)
+    return ans1 / ans2
+
+def A(m, n):
+    ans = 1.0
+    for i in range(m-n+1, m+1):
+        ans = ans * i
+    return ans 
+
+def main():
+    x1 = A(365, 30) / pow(365, 30)
+    x2 = C(30, 2) * A(365, 29) / pow(365, 30)
+    p = 1 - x1 - x2
+
+    print("p : {:4f}".format(p))
+    result = 1 - (C(5,1) * p * pow((1-p), 4) + C(5,0) * pow((1-p), 5))
+    print("result : {:4f}".format(result))
 
 if __name__ == "__main__":
-    a = [3,3,3,3,3]
-    print(bisearch(0, len(a)-1, 2, a))
+    main() 
