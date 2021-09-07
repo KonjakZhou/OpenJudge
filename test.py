1759bc09e291a8514522cade75ba1edd6db49fdf
KonJakZhou <cuttleb@163.com>
2021-09-07 10:43:35 +0800

    daily

--- test.py
+++ test.py
@@ -1,29 +1,18 @@
-def C(m, n):
-    ans1 = 1.0
-    for i in range(1, m+1):
-        ans1 = ans1 * i
-    ans2 = 1.0
-    for i in range(1, n+1):
-        ans2 = ans2 * i
-    for i in range(1, m-n+1):
-        ans2 = ans2 * i
+from math import exp
+from scipy.special import comb, perm
+from scipy.stats import poisson
+# perm(3,2)     #计算排列数    6
 
-    return ans1 / ans2
+# comb(3,2)   #计算组合数     3
 
-def A(m, n):
-    ans = 1.0
-    for i in range(m-n+1, m+1):
-        ans = ans * i
-    return ans 
-
-def main():
-    x1 = A(365, 30) / pow(365, 30)
-    x2 = C(30, 2) * A(365, 29) / pow(365, 30)
-    p = 1 - x1 - x2
-
-    print("p : {:4f}".format(p))
-    result = 1 - (C(5,1) * p * pow((1-p), 4) + C(5,0) * pow((1-p), 5))
-    print("result : {:4f}".format(result))
 
 if __name__ == "__main__":
-    main() 
+    ans = 0
+    for i in range(101, 107):
+        ans += poisson.pmf(i, 212 / 2 * 0.95 * 0.95)
+    print(ans)
+
+    ans = 0
+    for i in range(101, 107):
+        ans += comb(106, i) * pow(0.95 * 0.95, i) * pow(1 - 0.95 * 0.95, 106-i)
+    print(ans)
